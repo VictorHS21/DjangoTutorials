579edb56235ce9bfb45f9fc85fb1abf96596b575
VictorHS21 <victorhschmidt04@gmail.com>
2023-11-09 11:43:25 -0300

    Tutorial_REST_03 Completo

--- Tutorial_REST_02/posts/views.py
+++ Tutorial_REST_02/posts/views.py
@@ -1,12 +1,16 @@
-# posts/views.py
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Post
+from .permissions import IsAutorOrReadOnly
 from .serializers import PostSerializer
 
+
+# Create your views here.
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAutorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
